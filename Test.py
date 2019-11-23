29440cc9254ac01a47bd36e42068d635cc150979
Carlos Hernandez-Vaquero <>
2019-11-23 19:12:22 +0100

    Added .gitignore, more random changes in Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,11 @@
-print("TESTING!")
-ThisIsAString = 'strings everywehre'
+import sys
+
+print(sys.version)
+
+print("TESTING2!")
+ThisIsAString = "strings everywehre"
 print(ThisIsAString)
+a = 1
+b = 2
+c = 4
+print(a+b+c)
