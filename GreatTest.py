887be9899356f01382bbc045b5b1db376914c581
Carlos Hernandez-Vaquero <carlos.hernandez.vaquero@gmail.com>
2019-11-25 18:22:56 +0100

    Using docstrings

--- GreatTest.py
+++ GreatTest.py
@@ -12,7 +12,13 @@ print(a+b+c)
 
 
 def add(a, b):
+    """ This is some long description aka docstring. I am going to write some text to see what happens when it exceeds the limit. I can write really long sentences in the same line and see what happens.
+    Now I am in a new line. This text should be used as description for the function.
+    """
     return a+b
+
+
+print(add.__doc__)
 
 
 # Third additional change to see if the new commit is correctly linked in Github and correctly name shown
